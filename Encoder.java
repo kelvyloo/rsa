0deeb910859cc9f3bc195c9769d6b9012927d732
Kelvin Lu <kjlu11823@gmail.com>
2018-12-26 13:36:37 -0500

    Added comments and wrote the letter_to_int method

--- Encoder.java
+++ Encoder.java
@@ -1,10 +1,27 @@
 public class Encoder {
+    /**
+     * INSTANCE VARIABLES:
+     * Public variables
+     * @msg           Message to be encoded
+     * @public_key_n  A prime number n such that n = pq
+     * @public_key_e  Another prime number such that the private key d*e = 1 mod (p-1)(q-1)
+     *
+     * Private variables
+     * @string_coded_msg Original message but with each letter replaced by its alphanumeric value
+     * @digits_n         Length of the public key n
+     * @encoded_msg      Final encoded message after encryption
+     */
     String msg;
-    String ascii_msg;
     long public_key_n;
     long public_key_e;
-    long encoded_msg[] = new long[64];
 
+    private String string_coded_msg;
+    private int digits_n = String.valueOf(public_key_n).length();
+    private long encoded_msg[] = new long[64];
+
+    /**
+     * Class Constructor
+     * */
     public Encoder(String msg, long public_key_n, long public_key_e)
     {
         this.msg = msg;
@@ -14,6 +31,20 @@ public class Encoder {
 
     public String letter_to_int()
     {
-        return this.ascii_msg;
+        string_coded_msg = msg.toUpperCase();
+
+        for (int i = 0; i < msg.length(); i++) {
+            char letter = msg.charAt(i);
+            String alphabet_num;
+
+            if (letter >= 'A' && letter <= 'J') {
+                alphabet_num = "0" + (letter - 'A');
+            }
+            else {
+                alphabet_num = String.valueOf(letter - 'A');
+            }
+            string_coded_msg.replace(String.valueOf(letter), alphabet_num);
+        }
+        return string_coded_msg;
     }
 }
