e3dab6abaff3c41ff941f0b5d0c694e4c8f4a39a
Kelvin Lu <kjlu11823@gmail.com>
2018-12-27 12:19:59 -0500

    Documented mod_exp method

--- Main.java
+++ Main.java
@@ -1,50 +1,64 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int mod_exp(int b, int m, int n) {
-        // Modular exponentiation method
-        // Inputs are b, m, n
-        // Returns the value of x = b^n mod m where b, m, and n are positive ints
+    /**
+     * Fast modular exponentiation calculator
+     * Calculates the b^e mod m by converting the exponent into its binary seq
+     * (i.e. 117 = 1110101).
+     * Starting from the LSB if the bit is 1 then calculate base^bit_power mod m
+     * (i.e 5^1 mod 19, 5^2 mod 19, 5^4 mod 19, etc.)
+     * The product of those successive modular operations is the answer;
+     *
+     *
+     * @base  Base
+     * @mod   Modulo
+     * @exp   Exponent
+     *
+     * @return b^e mod m
+     * */
+    public static long mod_exp(long base, long mod, long exp) {
         String n_binary = "";
-        int x = 1;
-        int power;
+        long x = 1;
+        long power;
 
-        while(n > 0) {
-            int tmp = n % 2;
+        while(exp > 0) {
+            long tmp = exp % 2;
             n_binary = tmp + n_binary;
-            n /= 2;
+            exp /= 2;
         }
 
-        power = b % m;
+        power = base % mod;
 
         for (int i = 0; i < n_binary.length(); i++) {
             if (n_binary.charAt(i) == 1) {
-                x = (x * power) % m;
+                x = (x * power) % mod;
             }
-            power = power * power % m;
+            power = power * power % mod;
         }
 
         return x;
     }
+
     public static void main(String[] args) {
-        int b, m, n, x;
+        long b, m, n, x;
+        long encoded_msg[];
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("This a modular exponentiation calculator!");
+        System.out.println("This is a simple encoder!");
 
         System.out.printf("Please enter a number b: ");
-        b = scanner.nextInt();
+        b = scanner.nextLong();
 
         System.out.printf("Please enter a number m: ");
-        m = scanner.nextInt();
+        m = scanner.nextLong();
 
         System.out.printf("Please enter a number n: ");
-        n = scanner.nextInt();
+        n = scanner.nextLong();
 
         x = Main.mod_exp(b, m ,n);
 
         Encoder encoder = new Encoder("Hello", 362783, 19);
-        encoder.letter_to_int();
+        encoded_msg = encoder.encode();
 
         System.out.printf("%d^%d %% %d = %d", b, n, m, x);
     }
