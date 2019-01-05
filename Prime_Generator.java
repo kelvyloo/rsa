55687281d03cde035e7e6d2dcb3236dec1c733e8
Kelvin Lu <kjlu11823@gmail.com>
2019-01-05 16:42:22 -0500

    Added miller rabin primality test method

--- Prime_Generator.java
+++ Prime_Generator.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  * Prime number generator class
@@ -7,9 +8,14 @@ import java.util.Random;
  */
 public class Prime_Generator {
     int digits_n;
-    boolean is_prime;
+    private boolean is_prime;
     long prime_num;
 
+    /**
+     * Creates a random N-digit long potential prime number
+     *
+     * @return N digit long potential prime number
+     */
     private long number_gen() {
         Random digit = new Random();
         String num_str = "";
@@ -19,7 +25,7 @@ public class Prime_Generator {
         }
         char last_digit = num_str.charAt(digits_n - 1);
 
-        while (last_digit != '1' || last_digit != '3' || last_digit != '7') {
+        while (Integer.valueOf(last_digit) % 2 == 0 || last_digit == '5') {
             last_digit = (char) (digit.nextInt(7) + '0');
         }
         num_str = num_str.substring(0, digits_n - 1) + last_digit;
@@ -27,6 +33,67 @@ public class Prime_Generator {
         return Long.valueOf(num_str);
     }
 
+    /**
+     * Miller rabin primality test
+     * Takes any size number n and tests if the number is prime by running the
+     * Miller Rabin test for x number of witnesses (known prime numbers) below
+     * some threshold (this particular method will use 500).
+     *
+     * @param potential_prime number that will be run through primality test
+     *
+     * @return true if n is prime (99.99%) else false
+     */
+    private boolean miller_rabin(long potential_prime) {
+        int witnesses[] = {2, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                           53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
+                           109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+                           173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+                           233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283,
+                           293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359,
+                           367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431,
+                           433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491,
+                           499};
+        boolean prime;
+
+        if (potential_prime < 500) {
+            prime = IntStream.of(witnesses).anyMatch(x -> x == potential_prime);
+            return prime;
+        }
+
+        long d = (potential_prime-1) / 2;
+        int r = 1;
+
+        while (d % 2 == 0) {
+            d /= 2;
+            r = r++;
+        }
+
+        for (int witness : witnesses) {
+            long tmp;
+
+            tmp = Main.mod_exp(witness, potential_prime, d);
+
+            if (tmp != 1 && tmp != potential_prime-1) { break; }
+
+            for (int j = 0; j < r; j++) {
+                tmp = tmp * tmp % potential_prime;
+
+                if (tmp == 1) { return false; }
+
+                else if (tmp == potential_prime-1) { break; }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Class constructor for the prime generator
+     * Generates a random N-digit prime number and then runs it through
+     * a miller rabin primality test until a prime number is generated
+     *
+     * @param digits_n the physical length of the prime number being generated
+     */
     public Prime_Generator(int digits_n)
     {
         this.digits_n = digits_n;
@@ -34,6 +101,7 @@ public class Prime_Generator {
 
         while (!is_prime) {
             prime_num = number_gen();
+            is_prime = miller_rabin(prime_num);
         }
     }
 }
